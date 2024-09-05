1b8a764accb6e5a420edf24a30e0786740925110
Mahesh Mistry <maheshmistry879@gmail.com>
2024-09-05 18:08:32 +0200

    axios and error handling

---
+++ src/api/api.ts
@@ -0,0 +1,10 @@
+import { AxiosResponse } from "axios";
+import client from "./client";
+
+async function testApi(): Promise<AxiosResponse> {
+  return client.get("/test/");
+}
+
+export default {
+  testApi,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+
+// using services from openapi-ts generator was not properly working with custom error handling.
+// If worked properly, would be able to directly use the api endpoints from services.gen.ts as an axios client to access api
+
+// import { client } from './client';
+// import { handleError } from "./api/client";
+
+// client.setConfig({
+//   baseURL: import.meta.env.VITE_BASE_URL,
+//   headers: { [import.meta.env.VITE_AUTH_KEY]: import.meta.env.VITE_AUTH_VALUE}
+// });
+
+// client.instance.interceptors.response.use(response => response, handleError)
