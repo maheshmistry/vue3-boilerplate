97c319086b882e7bd050c85c78ce20ea053b4998
Mahesh Mistry <maheshmistry879@gmail.com>
2024-09-05 18:23:02 +0200

    openapi and config

---
+++ openapi-ts.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "@hey-api/openapi-ts";
+
+export default defineConfig({
+  client: "@hey-api/client-fetch",
+  //   Edit this path
+  input: "path/to/openapi.json",
+  output: "src/client",
+});
