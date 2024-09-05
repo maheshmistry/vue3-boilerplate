6877389073d1edb417621527adae98d18753482e
Mahesh Mistry <maheshmistry879@gmail.com>
2024-09-05 18:40:35 +0200

    pinia and test store

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
+
 import "./style.css";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.mount("#app");
 
 // using services from openapi-ts generator was not properly working with custom error handling.
 // If worked properly, would be able to directly use the api endpoints from services.gen.ts as an axios client to access api
---
+++ src/stores/boilerplateStore.ts
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useTestStore = defineStore("test", () => {
+  const test = ref<Array<string>>([]);
+
+  const test2 = () => {
+    return test;
+  };
+
+  function testFunc() {}
+  return { testFunc, test2 };
+});
